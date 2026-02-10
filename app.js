5ffc4e3756a0a048bb406fbada9b0ff26216e67e
Alexis Miens <a.miens63@yahoo.com>
2026-02-10 21:41:48 +0100

    Ajout de la partie accueil et de quelque commentaire

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // TODO : 
 // -Ajouter compétences, projets, contact, etc.
+// Completer les sections avec du contenu réel
 
 const { createApp } = Vue;
 // Je vais utiliser vue pour faire des boucle automatiques v-for pour les projets, compétences, etc. ça va creer les div automatiquement comme ça 
@@ -8,14 +9,28 @@ createApp({
         return {
             portfolio: {
                 nom: "Miens Alexis",
-                titre: "Développeur Web",
+                titre: "Développeur d'applications / Web",
+                soustitre: "blablabla",
                 apropos: {            
                     description: "Passionné par le développement web," +
                         "je crée des expériences numériques innovantes et performantes." +
                         "Avec une expertise en HTML, CSS, JavaScript et Vue.js, je transforme les idées en réalité." + 
                         "Découvrez mes projets et contactez-moi pour collaborer sur votre prochain projet web.",
                 },
-            }
+                compétences: [
+                    "Vue.js",
+                    "JavaScript",
+                    "HTML/CSS",
+                    "Node.js",
+                    "Git",
+                    "Sql/MySQL",
+                    "php",
+                    "Csharp NET",
+                ],
+                projets: [
+
+                ],
+            },
         };
     },
     mounted() {
